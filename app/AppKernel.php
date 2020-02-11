e9a27756fd5922c4299db99ad30e96f7dfb9e4cb
Jawadions <jawadi.ons.97@gmail.com>
2020-02-11 14:35:43 +0100

    actualiteBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new SolidarityBundle\SolidarityBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new ActualiteBundle\ActualiteBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
