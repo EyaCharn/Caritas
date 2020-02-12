f22d53bf95e23f824253284d86f38432de00d94d
hadilokkez <okkezhadil@gmail.com>
2020-02-12 20:12:02 +0100

    haddd

--- src/EvenementBundle/Controller/DefaultController.php
+++ src/EvenementBundle/Controller/DefaultController.php
@@ -6,8 +6,5 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('EvenementBundle:Default:index.html.twig');
-    }
+
 }
