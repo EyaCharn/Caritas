6da11d83d2642001df8c0a64934ae91b31b0e2be
nermine24 <nermine.belarbi@esprit.tn>
2020-02-18 17:10:26 +0100

    nermine wallh mat3almt

--- src/ReclamationBundle/Form/ReclamationType.php
+++ src/ReclamationBundle/Form/ReclamationType.php
@@ -3,6 +3,7 @@
 namespace ReclamationBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,15 @@ class ReclamationType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('description')->add('userclaimer');
+        $builder->add('description')
+            ->add('userclaimer',ChoiceType::class, [
+                'choices' => [
+                    'logement' => 'logement',
+                    'fourniture' => 'fourniture',
+                    'nouriture' => 'nouriture',
+                    'transport' => 'transport',
+                ],
+            ]);
     }/**
      * {@inheritdoc}
      */
